3732d008628d629e7a13ebfa3e09eb80726d0565
Jinglei Ren <jinglei@merico.dev>
2023-04-17 22:49:50 +0800

    Add docstrings and set linter in VS Code
    - Include module and function docstrings in _cli.py, test_cli.py
    - Discuss flake8, pylint; configure preferred linter in settings.json

--- devchat/_cli.py
+++ devchat/_cli.py
@@ -1,2 +1,9 @@
+"""
+This module contains the main function for the devchat CLI.
+"""
+
 def main():
+    """
+    The main function for the CLI. Prints "Hello, World!" when called.
+    """
     print("Hello World")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,15 @@
+"""
+This module contains tests for the devchat CLI.
+"""
+
 import io
-import sys
 from contextlib import redirect_stdout
 from devchat._cli import main
 
 def test_main():
+    """
+    Test the main function of the CLI. Ensures it prints "Hello, World!" as expected.
+    """
     buffer = io.StringIO()
     with redirect_stdout(buffer):
         main()
