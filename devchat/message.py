d1bcca6657cf4d8dc159544c6832311e02efa40a
Jinglei Ren <jinglei@merico.dev>
2023-04-18 20:09:38 +0800

    Create Message class for role, content, and name
    This commit introduces a Message class that holds the role
    (system, user, or assistant), content, and an optional name for the
    author. It also provides a convenient way to generate an array in the
    required format for the OpenAI APIs. The class follows best practices
    for class design and constructor usage.

---
+++ devchat/message.py
@@ -0,0 +1,71 @@
+"""This module contains the Message class for working with OpenAI chat API messages."""
+
+
+from typing import Optional
+
+class Message:
+    """A class to represent a message in a conversation with OpenAI's chat API.
+
+    Attributes:
+        role (str): The role of the author of the message. One of 'system', 'user', or 'assistant'.
+        content (str): The contents of the message.
+        name (str, optional): The name of the author of the message. May contain a-z, A-Z, 0-9, and
+                              underscores, with a maximum length of 64 characters.
+    """
+
+    def __init__(self, role: str, content: str, name: Optional[str] = None):
+        self.role = role
+        self.content = content
+        self.name = name
+
+        if not self._validate_role():
+            raise ValueError("Invalid role. Must be one of 'system', 'user', or 'assistant'.")
+
+        if not self._validate_content():
+            raise ValueError("Content must not be empty or contain only whitespaces.")
+
+        if not self._validate_name():
+            raise ValueError("Invalid name. Must contain a-z, A-Z, 0-9, and underscores, "
+                             "with a maximum length of 64 characters.")
+
+    def _validate_role(self) -> bool:
+        """Validate the role attribute.
+
+        Returns:
+            bool: True if the role is valid, False otherwise.
+        """
+        return self.role in ["system", "user", "assistant"]
+
+    def _validate_content(self) -> bool:
+        """Validate the content attribute.
+
+        Returns:
+            bool: True if the content is valid, False otherwise.
+        """
+        return bool(self.content and self.content.strip())
+
+    def _validate_name(self) -> bool:
+        """Validate the name attribute.
+
+        Returns:
+            bool: True if the name is valid or None, False otherwise.
+        """
+        if self.name is None:
+            return True
+        if not self.name.strip():
+            return False
+        return len(self.name) <= 64 and self.name.replace("_", "").isalnum()
+
+    def to_dict(self) -> dict:
+        """Convert the Message object to a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the Message object.
+        """
+        message_dict = {
+            "role": self.role,
+            "content": self.content,
+        }
+        if self.name is not None:
+            message_dict["name"] = self.name
+        return message_dict
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,15 +1,8 @@
-"""
-This module contains tests for the devchat CLI.
-"""
-
 import io
 from contextlib import redirect_stdout
 from devchat._cli import main
 
 def test_main():
-    """
-    Test the main function of the CLI. Ensures it prints "Hello, World!" as expected.
-    """
     buffer = io.StringIO()
     with redirect_stdout(buffer):
         main()
