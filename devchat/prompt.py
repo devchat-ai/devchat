113fc0861a76a623597bab00f59320dcee88faf1
Jinglei Ren <jinglei@merico.dev>
2023-04-22 22:42:37 +0800

    Update Prompt's append_response and add test
    - Refactor Prompt's append_response to handle dict for messages
    - Validate message role when appending content
    - Move model validation to a separate private function
    - Add pytest fixture for loading stream_responses from files
    - Update test_append_response to use fixture and validate output

--- devchat/prompt.py
+++ devchat/prompt.py
@@ -1,6 +1,7 @@
 import json
 from devchat.message import Message
 
+
 class Prompt:
     """
     A class to represent a prompt and its corresponding response from the chat API.
@@ -20,7 +21,7 @@ class Prompt:
         self.response_time = None
         self.request_tokens = None
         self.response_tokens = None
-        self.messages = None
+        self.messages = {}
 
     def set_response(self, response_str: str):
         """
@@ -30,10 +31,7 @@ class Prompt:
             response_str (str): The JSON-formatted response string from the chat API.
         """
         response_data = json.loads(response_str)
-
-        if not response_data['model'].startswith(self.model):
-            raise ValueError(f"Model mismatch: expected '{self.model}', "
-                             f"got '{response_data['model']}'")
+        self._validate_model(response_data)
 
         self.response_meta = {
             'id': response_data['id'],
@@ -47,3 +45,45 @@ class Prompt:
             Message.from_dict(choice['message'])
             for choice in response_data['choices']
         ]
+
+    def append_response(self, response_str: str):
+        """
+        Append the content of a streamed API response to the existing messages.
+
+        Args:
+            response_str (str): The JSON-formatted response string from the chat API.
+        """
+        response_data = json.loads(response_str)
+        self._validate_model(response_data)
+
+        for choice in response_data['choices']:
+            delta = choice.get('delta')
+            index = choice.get('index')
+
+            if delta is None:
+                raise ValueError("The 'delta' field is missing in the response.")
+
+            if not delta:
+                # An empty delta indicates the end of the message
+                continue
+
+            role = delta.get('role')
+            content = delta.get('content')
+
+            if role is not None:
+                if index not in self.messages:
+                    self.messages[index] = Message(role)
+            elif content is not None:
+                if index in self.messages:
+                    message = self.messages[index]
+                    if message.content is None:
+                        message.content = content
+                    else:
+                        message.content += content
+                else:
+                    raise ValueError(f"Role information for index {index} is missing.")
+
+    def _validate_model(self, response_data: dict):
+        if not response_data['model'].startswith(self.model):
+            raise ValueError(f"Model mismatch: expected '{self.model}', "
+                             f"got '{response_data['model']}'")
