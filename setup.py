0075fbd9d2144eca43e3cc886d1fc3a4b0e0225c
Jinglei Ren <jinglei@merico.dev>
2023-04-23 21:13:16 +0800

    Release version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="devchat",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
