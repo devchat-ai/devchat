51e59be9551dd42428def9e8f814393d6819d509
Jinglei Ren <jinglei@merico.dev>
2023-04-23 21:44:15 +0800

    Add install requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="devchat",
     version="0.1.1",
     packages=find_packages(),
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "devchat = devchat._cli:main",
