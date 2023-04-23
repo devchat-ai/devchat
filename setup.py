090878f3e89867ba2d49111c42e1f7466f3e932f
Jinglei Ren <jinglei@merico.dev>
2023-04-23 21:49:27 +0800

    Release version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="devchat",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
