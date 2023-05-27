b68a84283a182fc5cf669f401477127a24d99967
Jinglei Ren <jinglei@merico.dev>
2023-05-27 23:18:01 +0800

    Increase version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = read_file("README.md")
 
 setup(
     name="devchat",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
