48fab3dd445d5da24f7210fac070e8fd87ebb789
Jinglei Ren <jinglei@merico.dev>
2023-06-06 21:17:51 +0800

    Add purge_topics.py to scripts

---
+++ scripts/purge_topics.py
@@ -0,0 +1,22 @@
+import sys
+from tinydb import TinyDB
+
+
+def remove_topic_table(file_path: str):
+    try:
+        db = TinyDB(file_path)
+        if 'topics' in db.tables():
+            db.drop_table('topics')
+            print("The 'topics' table has been removed.")
+        else:
+            print("The file does not contain a 'topics' table.")
+    except Exception as exc:
+        print(f"Error: {exc}. The file is not a valid TinyDB file or could not be processed.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python remove_topic_table.py <file_path>")
+        sys.exit(1)
+
+    remove_topic_table(sys.argv[1])
