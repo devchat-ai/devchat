bc78c513010573cb083b5c97c50400ae28237baf
Jinglei Ren <jinglei@merico.dev>
2023-07-05 20:15:59 +0800

    Update request timeout and add urllib3 dependency
    - Increased the request timeout in OpenAIChatConfig from 32 to 50.
    - Added urllib3<2.0 to the requirements.txt file.
    - Bumped up the version number in setup.py from 0.1.13 to 0.1.14.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = read_file("README.md")
 
 setup(
     name="devchat",
-    version="0.1.13",
+    version="0.1.14",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
