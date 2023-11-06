7a7effeae02f8365c8a924f8ddbf0931f96ea8ae
bobo.yang <bobo.yang@merico.dev>
2023-11-06 16:34:07 +0800

    handle git missing error

--- devchat/_cli/run.py
+++ devchat/_cli/run.py
@@ -6,11 +6,11 @@ from typing import List
 import rich_click as click
 from git import Repo, GitCommandError
 from devchat._cli.utils import init_dir, handle_errors, valid_git_repo, clone_git_repo
+from devchat._cli.utils import download_and_extract_workflow
 from devchat.engine import Namespace, CommandParser, RecursivePrompter
 from devchat.utils import get_logger
 
 logger = get_logger(__name__)
-
 
 @click.command(
     help="The 'command' argument is the name of the command to run or get information about.")
@@ -44,7 +44,11 @@ def run(command: str, list_flag: bool, recursive_flag: bool, update_sys_flag: bo
                 'https://gitee.com/devchat-ai/workflows.git',
                 'https://github.com/devchat-ai/workflows.git'
             ]
-            _clone_or_pull_git_repo(sys_dir, git_urls)
+            zip_urls = [
+                'https://gitlab.com/devchat-ai/workflows/-/archive/main/workflows-main.zip',
+                'https://codeload.github.com/devchat-ai/workflows/zip/refs/heads/main'
+            ]
+            _clone_or_pull_git_repo(sys_dir, git_urls, zip_urls)
             return
 
         if list_flag:
@@ -72,13 +76,23 @@ def run(command: str, list_flag: bool, recursive_flag: bool, update_sys_flag: bo
             return
 
 
-def _clone_or_pull_git_repo(target_dir: str, repo_urls: List[str]):
+def _clone_or_pull_git_repo(target_dir: str, repo_urls: List[str], zip_urls: List[str]):
     """
     Clone a Git repository to a specified location, or pull it if it already exists.
 
     :param target_dir: The path where the repository should be cloned.
     :param repo_urls: A list of possible Git repository URLs.
     """
+    if shutil.which('git') is None:
+        # If Git is not installed, download and extract the workflow
+        for url in zip_urls:
+            try:
+                download_and_extract_workflow(url, target_dir)
+                break
+            except Exception as e:
+                logger.exception("Failed to download and extract workflow: %s", e)
+        return
+
     if os.path.exists(target_dir):
         if valid_git_repo(target_dir, repo_urls):
             try:
