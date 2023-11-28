f7e8b63197dbd50b025f68b9f4304fab884c6db7
bobo.yang <bobo.yang@merico.dev>
2023-11-28 17:58:23 +0800

    fix test error

--- devchat/_cli/prompt.py
+++ devchat/_cli/prompt.py
@@ -26,15 +26,15 @@ from devchat._cli.utils import handle_errors, init_dir, get_model_config
               help='Path to a JSON file with functions for the prompt.')
 @click.option('-n', '--function-name',
               help='Specify the function name when the content is the output of a function.')
-@click.option('-s', '--store', is_flag=True, default=False,
+@click.option('-s', '--store', is_flag=True, default=True, required=False,
               help='Save the conversation to the store.')
-@click.option('-a', '--auto', is_flag=True, default=False,
+@click.option('-a', '--auto', is_flag=True, default=False, required=False,
               help='Answer question by function-calling.')
 def prompt(content: Optional[str], parent: Optional[str], reference: Optional[List[str]],
            instruct: Optional[List[str]], context: Optional[List[str]],
            model: Optional[str], config_str: Optional[str] = None,
            functions: Optional[str] = None, function_name: Optional[str] = None,
-           store: Optional[bool] = False, auto: Optional[bool] = False):
+           store: Optional[bool] = True, auto: Optional[bool] = False):
     """
     This command performs interactions with the specified large language model (LLM)
     by sending prompts and receiving responses.
