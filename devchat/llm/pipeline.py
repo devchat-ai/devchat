e6c69dc111ca21ce086d77045eb6c7f64bebc527
bobo.yang <bobo.yang@merico.dev>
2024-02-07 16:07:35 +0800

    Refactor pipeline function to handle parallel execution

--- devchat/llm/pipeline.py
+++ devchat/llm/pipeline.py
@@ -62,7 +62,8 @@ def pipeline(*funcs):
     def wrapper(*args, **kwargs):
         for index, func in enumerate(funcs):
             if index > 0:
-                if isinstance(args, Dict) and args.get("__type__", None) == "parallel":  # pylint: disable=E1101
+                # pylint: disable=E1101
+                if isinstance(args, Dict) and args.get("__type__", None) == "parallel":
                     args = func(*args["value"]) # pylint: disable=E1126
                 else:
                     args = func(args)
