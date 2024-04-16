ff819e35b438cfcdfb1c3d8f5f465be26de133ac
kagami <mingjing@merico.dev>
2024-04-16 21:01:43 +0800

    Add workflow & step implementation

---
+++ devchat/workflow/step.py
@@ -0,0 +1,199 @@
+# pylint: disable=invalid-name
+
+import os
+import sys
+import threading
+import subprocess
+import shlex
+import json
+from typing import Dict, Tuple
+from enum import Enum
+from .schema import WorkflowConfig, RuntimeParameter
+from .path import WORKFLOWS_BASE
+
+
+class BuiltInVars(str, Enum):
+    """
+    Built-in variables within the workflow step command.
+    """
+
+    devchat_python = "$devchat_python"
+    command_path = "$command_path"
+    user_input = "$input"
+    workflow_python = "$workflow_python"
+
+
+class BuiltInEnvs(str, Enum):
+    """
+    Built-in environment variables for the step subprocess.
+    """
+
+    llm_model = "LLM_MODEL"
+    parent_hash = "PARENT_HASH"
+    context_contents = "CONTEXT_CONTENTS"
+
+
+class WorkflowStep:
+    # TODO: algin syntax with the documentation
+    def __init__(self, **kwargs):
+        """
+        Initialize a workflow step with the given configuration.
+        """
+        self._kwargs = kwargs
+
+    @property
+    def command_raw(self) -> str:
+        """
+        The raw command string from the config.
+        """
+        return self._kwargs.get("run", "")
+
+    def _setup_env(
+        self, wf_config: WorkflowConfig, rt_param: RuntimeParameter
+    ) -> Dict[str, str]:
+        """
+        Setup the environment variables for the subprocess.
+
+        TODO: any validation or error handling?
+        """
+        command_raw = self.command_raw
+
+        env = os.environ.copy()
+
+        # set PYTHONPATH for the subprocess
+        # TODO: import env vars from envs.py
+        python_path = env.get("PYTHONPATH", "")
+        devchat_python_path = env.get("DEVCHAT_PYTHONPATH", python_path)
+        new_paths = [WORKFLOWS_BASE]
+        if (BuiltInVars.devchat_python in command_raw) and devchat_python_path:
+            # only add devchat pythonpath when it's used in the command
+            new_paths.append(devchat_python_path)
+        # TODO: set workflow_python path
+        # if (BuiltInVars.workflow_python in command_raw) and wf_config.workflow_python:
+
+        env["PYTHONPATH"] = os.pathsep.join(new_paths)
+        env[BuiltInEnvs.llm_model] = rt_param.model_name or ""
+        env[BuiltInEnvs.parent_hash] = rt_param.parent_hash or ""
+        env[BuiltInEnvs.context_contents] = ""
+        if rt_param.history_messages:
+            # convert dict to json string
+            env[BuiltInEnvs.context_contents] = json.dumps(rt_param.history_messages)
+
+        return env
+
+    def _validate_and_interpolate(
+        self, wf_config: WorkflowConfig, rt_param: RuntimeParameter
+    ) -> str:
+        """
+        Validate the step configuration and interpolate variables in the command.
+
+        Return the interpolated command string.
+        """
+        command_raw = self.command_raw
+
+        # if the command_raw use $workflow_python,
+        # it must be set in workflow config
+        if BuiltInVars.workflow_python in command_raw:
+            if not rt_param.workflow_python:
+                raise ValueError(
+                    "The command uses $workflow_python, "
+                    "but the workflow_python is not set yet."
+                )
+
+        # variable interpolation in the command
+        command = (
+            command_raw.replace(BuiltInVars.command_path, wf_config.root_path)
+            .replace(BuiltInVars.devchat_python, rt_param.devchat_python)
+            .replace(BuiltInVars.user_input, rt_param.user_input)
+            .replace(BuiltInVars.workflow_python, rt_param.workflow_python)
+        )
+
+        return command
+
+    def run(
+        self, wf_config: WorkflowConfig, rt_param: RuntimeParameter
+    ) -> Tuple[int, str, str]:
+        """
+        Run the step in a subprocess.
+
+        Returns the return code, stdout, and stderr.
+
+        TODO: any validation or error handling?
+        """
+        # command_raw = self.command_raw
+
+        # setup the environment variables
+        env = self._setup_env(wf_config, rt_param)
+
+        # print(f"\n\n- env: \n\n")
+        # for k, v in env.items():
+        #     print(f"\n\n- {k}: \n\n```\n{v}\n```\n\n")
+
+        # validate the command first
+        # variable interpolation in the command
+        # command = (
+        #     command_raw.replace(BuiltInVars.command_path, wf_config.root_path)
+        #     .replace(BuiltInVars.devchat_python, rt_param.devchat_python)
+        #     .replace(BuiltInVars.user_input, rt_param.user_input)
+        # )
+        command = self._validate_and_interpolate(wf_config, rt_param)
+
+        # print(f"\n\n- command_raw: {command_raw}")
+        # print(f"- command: {command}\n\n")
+        # print("\n\n```\n\n")
+        # print(shlex.split(command))
+        # print("\n\n")
+        # for k, v in env.items():
+        #     print(f"\n- {k}: {type(v)}")
+        #     print(f"  {v}")
+
+        # print("\n\n```\n\n")
+
+        def _pipe_reader(pipe, data, out_file):
+            """
+            Read from the pipe, then write and save the data.
+            """
+            while pipe:
+                pipe_data = pipe.read(1)
+                if pipe_data == "":
+                    break
+                data["data"] += pipe_data
+                print(pipe_data, end="", file=out_file, flush=True)
+
+        with subprocess.Popen(
+            shlex.split(command),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=env,
+            text=True,
+        ) as proc:
+            stdout_data, stderr_data = {"data": ""}, {"data": ""}
+            stdout_thread = threading.Thread(
+                target=_pipe_reader, args=(proc.stdout, stdout_data, sys.stdout)
+            )
+            stderr_thread = threading.Thread(
+                target=_pipe_reader, args=(proc.stderr, stderr_data, sys.stderr)
+            )
+            stdout_thread.start()
+            stderr_thread.start()
+            stdout_thread.join()
+            stderr_thread.join()
+
+            proc.wait()
+            return_code = proc.returncode
+            return return_code, stdout_data["data"], stderr_data["data"]
+
+        # print(f"\n\n-----\n\n## envs: \n\n")
+        # for k, v in env.items():
+        #     print(f"- {k}: {v}")
+        # print("\n\n\n\n")
+
+        # with subprocess.Popen(
+        #     shlex.split(command),
+        #     stdout=subprocess.PIPE,
+        #     stderr=subprocess.PIPE,
+        # ) as proc:
+        #     out, err = proc.communicate()
+
+        #     print(f"\n\n- out: {out}")
+        #     print(f"- err: {err}\n\n")
---
+++ devchat/workflow/workflow.py
@@ -0,0 +1,163 @@
+# pylint: disable=invalid-name
+
+import os
+import sys
+from typing import Optional, Tuple, List, Dict
+import oyaml as yaml
+from .step import WorkflowStep
+from .schema import WorkflowConfig, RuntimeParameter
+from .path import (
+    PrioritizedWorkflows,
+    COMMAND_FILENAME,
+)
+from .env_manager import PyEnvManager
+
+
+class Workflow:
+    # TODO: align args and others with the documentation
+
+    TRIGGER_PREFIX = "="
+
+    def __init__(self, config: WorkflowConfig):
+        self._config = config
+
+        self._runtime_param = None
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def runtime_param(self):
+        return self._runtime_param
+
+    @staticmethod
+    def parse_trigger(user_input: str) -> Tuple[Optional[str], Optional[str]]:
+        """
+        Check if the user input should trigger a workflow.
+        Return a tuple of (workflow_name, the input without workflow trigger).
+
+        User input is considered a workflow trigger if it starts with the Workflow.PREFIX.
+        The workflow name is the first word after the prefix.
+        """
+        striped = user_input.strip()
+        if not striped:
+            return None, user_input
+        if striped[0] != Workflow.TRIGGER_PREFIX:
+            return None, user_input
+
+        workflow_name = striped.split()[0][1:]
+
+        # remove the trigger prefix and the workflow name
+        actual_input = user_input.replace(
+            f"{Workflow.TRIGGER_PREFIX}{workflow_name}", "", 1
+        )
+        return workflow_name, actual_input
+
+    @staticmethod
+    def load(workflow_name: str) -> Optional["Workflow"]:
+        """
+        Load a workflow from the command.yml by name.
+        A workflow name is the relative path of command.yml
+        to the /workflows dir joined by "."
+        e.g
+        - "unit_tests": means the command file of the workflow is unit_tests/command.yml
+        - "commit.en": means the command file is commit/en/command.yml
+        - "pr.review.zh": means the command file is pr/review/zh/command.yml
+        """
+        path_parts = workflow_name.split(".")
+        if len(path_parts) < 1:
+            return None
+        # path_parts.append(COMMAND_FILENAME)
+        rel_path = os.path.join(*path_parts)
+
+        found = False
+        workflow_dir = ""
+        for wf_dir in PrioritizedWorkflows:
+            yaml_file = os.path.join(wf_dir, rel_path, COMMAND_FILENAME)
+            if os.path.exists(yaml_file):
+                workflow_dir = wf_dir
+                found = True
+                break
+        if not found:
+            return None
+
+        # Load and override yaml conf in top-down order
+        config_dict = {}
+        for i in range(len(path_parts)):
+            cur_path = os.path.join(workflow_dir, *path_parts[: i + 1])
+            cur_yaml = os.path.join(cur_path, COMMAND_FILENAME)
+
+            if os.path.exists(cur_yaml):
+                with open(cur_yaml, "r", encoding="utf-8") as file:
+                    yaml_content = file.read()
+                    cur_conf = yaml.safe_load(yaml_content)
+                    cur_conf["root_path"] = cur_path
+
+                # convert relative path to absolute path for dependencies file
+                if cur_conf.get("workflow_python", {}).get("dependencies"):
+                    rel_dep = cur_conf["workflow_python"]["dependencies"]
+                    abs_dep = os.path.join(cur_path, rel_dep)
+                    cur_conf["workflow_python"]["dependencies"] = abs_dep
+
+                config_dict.update(cur_conf)
+
+        config = WorkflowConfig.parse_obj(config_dict)
+
+        if config.workflow_python and config.workflow_python.env_name is None:
+            # use the workflow name as the env name if not set
+            config.workflow_python.env_name = workflow_name
+
+        return Workflow(config)
+
+    def setup(
+        self,
+        model_name: Optional[str],
+        user_input: Optional[str],
+        history_messages: Optional[List[Dict]],
+        parent_hash: Optional[str],
+    ):
+        """
+        Setup the workflow with the runtime parameters and env variables.
+        """
+        workflow_py = ""
+        if self.config.workflow_python:
+            # TODO: 有没有更好的时机判断方法？既保证运行时一定安装了依赖、又不用每次都检查？
+            # TODO: 只在插件(IDE)启动后workflow第一次使用时ensure环境和依赖？
+            # Create workflow python env if set in the config
+            pyconf = self.config.workflow_python
+
+            manager = PyEnvManager()
+            workflow_py = manager.ensure(pyconf.env_name, pyconf.version)
+
+            # r_file = os.path.join(self.config.root_path, pyconf.dependencies)
+            r_file = pyconf.dependencies
+            # print(f"\n\n requirements file: {r_file} \n\n")
+            _ = manager.install(pyconf.env_name, r_file)
+            # print(f"\n\ninstall result: {p}")
+
+        runtime_param = {
+            # from user interaction
+            "model_name": model_name,
+            "user_input": user_input,
+            "history_messages": history_messages,
+            "parent_hash": parent_hash,
+            # from user setting or system
+            # TODO: what if the user has not set the python path?
+            "devchat_python": sys.executable,
+            "workflow_python": workflow_py,
+        }
+
+        self._runtime_param = RuntimeParameter.parse_obj(runtime_param)
+
+    def run_steps(self):
+        """
+        Run the steps of the workflow.
+        """
+        steps = self.config.steps
+
+        for s in steps:
+            step = WorkflowStep(**s)
+            step.run(self.config, self.runtime_param)
+
+            print("\n\n")
