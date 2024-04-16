587a486e14ef08dce27fffd9e48425d70334dcca
kagami <mingjing@merico.dev>
2024-04-16 17:18:08 +0800

    Add envs and path settings

---
+++ devchat/workflow/envs.py
@@ -0,0 +1,10 @@
+"""
+Explicitly define the environment variables used in the workflow engine.
+"""
+import os
+
+PYTHON_PATH = os.environ.get("PYTHONPATH", "")
+DEVCHAT_PYTHON_PATH = os.environ.get("DEVCHAT_PYTHONPATH", PYTHON_PATH)
+
+# the path to the mamba binary
+MAMBA_BIN_PATH = os.environ.get("MAMBA_BIN_PATH", "")
---
+++ devchat/workflow/path.py
@@ -0,0 +1,34 @@
+import os
+
+# -------------------------------
+# devchat basic paths
+# -------------------------------
+USE_DIR = os.path.expanduser("~")
+CHAT_DIR = os.path.join(USE_DIR, ".chat")
+
+
+# -------------------------------
+# workflow scripts paths
+# -------------------------------
+WORKFLOWS_BASE_NAME = "new_wf"
+WORKFLOWS_BASE = os.path.join(CHAT_DIR, WORKFLOWS_BASE_NAME)  # TODO: a temporary name
+
+MERICO_WORKFLOWS = os.path.join(WORKFLOWS_BASE, "merico", "workflows")
+CUSTOM_WORKFLOWS = os.path.join(WORKFLOWS_BASE, "custom", "workflows")
+COMMUNITY_WORKFLOWS = os.path.join(WORKFLOWS_BASE, "community", "workflows")
+
+COMMAND_FILENAME = "command.yml"
+
+# the priority order of the workflows when naming conflicts
+PrioritizedWorkflows = [
+    CUSTOM_WORKFLOWS,
+    MERICO_WORKFLOWS,
+    COMMUNITY_WORKFLOWS,
+]
+
+
+# -------------------------------
+#  Python environments paths
+# -------------------------------
+MAMBA_ROOT = os.path.join(CHAT_DIR, "mamba")
+MAMBA_PY_ENVS = os.path.join(MAMBA_ROOT, "envs")
