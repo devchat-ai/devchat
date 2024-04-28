2eeb917c81108235b04ef276cb3d3d7495dec7fe
kagami <mingjing@merico.dev>
2024-04-28 20:40:41 +0800

    Implement command: workflow list

--- devchat/workflow/cli.py
+++ devchat/workflow/cli.py
@@ -1,6 +1,6 @@
 import rich_click as click
 from devchat.workflow.command.update import update
-from devchat.workflow.command.list import list_workflows
+from devchat.workflow.command.list import list_cmd
 from devchat.workflow.command.env import env
 
 
@@ -12,7 +12,7 @@ def workflow():
 
 
 workflow.add_command(update)
-workflow.add_command(list_workflows)
+workflow.add_command(list_cmd)
 workflow.add_command(env)
 
 
