ec9e62c3ef3d67a6a5b6350d8f356da44ec5d710
kagami <mingjing@merico.dev>
2024-04-28 20:42:11 +0800

    Implement command: workflow config

--- devchat/workflow/cli.py
+++ devchat/workflow/cli.py
@@ -2,11 +2,10 @@ import rich_click as click
 from devchat.workflow.command.update import update
 from devchat.workflow.command.list import list_cmd
 from devchat.workflow.command.env import env
+from devchat.workflow.command.config import config_cmd
 
 
-@click.group(
-    help="CLI for devchat workflow engine."
-)
+@click.group(help="CLI for devchat workflow engine.")
 def workflow():
     pass
 
@@ -14,6 +13,7 @@ def workflow():
 workflow.add_command(update)
 workflow.add_command(list_cmd)
 workflow.add_command(env)
+workflow.add_command(config_cmd)
 
 
 if __name__ == "__main__":
