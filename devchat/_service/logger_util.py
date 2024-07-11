adc96eee018ec0b4a5201394b16acf4fe0312cbc
kagami <mingjing@merico.dev>
2024-07-11 10:37:21 +0800

    Unify logging settings

---
+++ devchat/_service/logger_util.py
@@ -0,0 +1,64 @@
+import os
+import logging
+import sys
+
+from gunicorn.app.base import BaseApplication
+from gunicorn.glogging import Logger
+from loguru import logger
+
+LOG_LEVEL = logging.getLevelName(os.environ.get("LOG_LEVEL", "DEBUG"))
+JSON_LOGS = True if os.environ.get("JSON_LOGS", "0") == "1" else False
+WORKERS = int(os.environ.get("GUNICORN_WORKERS", "5"))
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # find caller from where originated the logged message
+        frame, depth = sys._getframe(6), 6
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+class StubbedGunicornLogger(Logger):
+    def setup(self, cfg):
+        handler = logging.NullHandler()
+        self.error_logger = logging.getLogger("gunicorn.error")
+        self.error_logger.addHandler(handler)
+        self.access_logger = logging.getLogger("gunicorn.access")
+        self.access_logger.addHandler(handler)
+        self.error_logger.setLevel(LOG_LEVEL)
+        self.access_logger.setLevel(LOG_LEVEL)
+
+
+class StandaloneApplication(BaseApplication):
+    """Our Gunicorn application."""
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
--- devchat/_service/main.py
+++ devchat/_service/main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 from devchat._service.route import router
 
+import logging
+import sys
+from loguru import logger
+
+from devchat._service.logger_util import (
+    InterceptHandler,
+    JSON_LOGS,
+    LOG_LEVEL,
+    WORKERS,
+    StandaloneApplication,
+    StubbedGunicornLogger,
+)
+
 api_app = FastAPI(
     title="DevChat Local Service",
 )
@@ -15,3 +28,37 @@ api_app.include_router(router)
 
 # https://www.reddit.com/r/FastAPI/comments/170awhx/mount_socketio_to_fastapi/
 # https://github.com/miguelgrinberg/python-socketio/blob/main/examples/server/asgi/fastapi-fiddle.py
+
+
+if __name__ == "__main__":
+    intercept_handler = InterceptHandler()
+    # logging.basicConfig(handlers=[intercept_handler], level=LOG_LEVEL)
+    # logging.root.handlers = [intercept_handler]
+    logging.root.setLevel(LOG_LEVEL)
+
+    seen = set()
+    for name in [
+        *logging.root.manager.loggerDict.keys(),
+        "gunicorn",
+        "gunicorn.access",
+        "gunicorn.error",
+        "uvicorn",
+        "uvicorn.access",
+        "uvicorn.error",
+    ]:
+        if name not in seen:
+            seen.add(name.split(".")[0])
+            logging.getLogger(name).handlers = [intercept_handler]
+
+    logger.configure(handlers=[{"sink": sys.stdout, "serialize": JSON_LOGS}])
+
+    options = {
+        "bind": "0.0.0.0:22222",
+        "workers": WORKERS,
+        "accesslog": "-",
+        "errorlog": "-",
+        "worker_class": "uvicorn.workers.UvicornWorker",
+        "logger_class": StubbedGunicornLogger,
+    }
+
+    StandaloneApplication(api_app, options).run()
